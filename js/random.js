44c1228c066c70c7922a84752b7d45a8267fbc24
rafaelmerlotto <rafaelmerlotto@gmail.com>
2023-02-08 02:13:24 +0100

    app-js

---
+++ js/random.js
@@ -0,0 +1,15 @@
+
+
+const getInput = () =>{
+   let valorInput =  Math.round(Math.random() * 999) + 1;
+   document.getElementById('valueInput').innerHTML = valorInput;
+/*    if(valorInput > 1000 || valorInput < 0 ){
+      alert('ops');
+      return;
+   } */
+}
+
+
+
+
+
